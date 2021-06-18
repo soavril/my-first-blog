8ef4bcb2e5a5947c732417bebcadaa430f201316
soavril <soavril96@gmail.com>
2021-06-18 14:31:15 +0900

    로그인 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', views.LoginView, name='login'),
+    url(r'^accounts/logout/$', views.LogoutView, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
